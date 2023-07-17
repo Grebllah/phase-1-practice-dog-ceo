a8142f27691c2f9cab8bd216f7aba729e2e035d0
James Hallberg <Grebllah81788@gmail.com>
2023-07-17 16:42:50 -0400

    complete dog ceo lab

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 console.log('%c HI', 'color: firebrick')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
 const breedUrl = "https://dog.ceo/api/breeds/list/all";
+let breeds = ""
 
 addEventListener("DOMContentLoaded", function(){
+
+    const breedDropdown = document.querySelector("#breed-dropdown")
+    const breedsList = document.querySelector("#dog-breeds")
+    const lis = document.querySelectorAll("li")
+    
     fetch(imgUrl)
     .then(function (response) {
         const data = response.json();
@@ -14,23 +20,36 @@ addEventListener("DOMContentLoaded", function(){
             const dogImg = data.message[i]
             const img = dogImgs.appendChild(document.createElement("img"))
             img.src = dogImg
-            img.height = 250
+            img.height = 100
         };
     });
-});
-
-addEventListener("DOMContentLoaded", function(){
+    
     fetch(breedUrl)
         .then(function (response) {
             const breedData = response.json();
             return breedData;
         })
         .then(function(data){
-            console.log(data)
-            const breeds = document.querySelector("#dog-breeds")
-            for (const breed of Object.keys(data.message)) {
-                const breedLi = breeds.appendChild(document.createElement("li")).innerText = breed
-                }
+            breeds = Object.keys(data.message)
+            for (const breed of breeds) {
+                const breedLi = document.createElement("li")
+                breedsList.appendChild(breedLi).innerText = breed
+                breedLi.addEventListener("click", function(e){
+                    e.target.style.color = "firebrick"
+                })
+            }
+        })
+    
+    breedDropdown.addEventListener("change", function(e){
+        breedsList.innerHTML = ""
+        const filteredBreeds = breeds.filter(breedName => breedName[0] === e.target.value)
+        for (const breed of filteredBreeds) {
+            const breedLi = document.createElement("li")
+            breedsList.appendChild(breedLi).innerText = breed
+            breedLi.addEventListener("click", function(e){
+                e.target.style.color = "firebrick"
             })
-});
+        }
+    })
 
+}); 
